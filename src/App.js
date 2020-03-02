84f6cf0ebce01842b9dd7bda7e3e6852e55f2e1b
Anton <antonbendrik1993@gmail.com>
2020-03-02 16:11:08 +0200

    added header,main,footer files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './images/logo.png';
 import './App.scss';
-
+import Header from './header/Header'
+import Main from './main/Main'
+import Footer from './footer/Footer';
 function App() {
   return (
     <div className="wrapper">
       <div className="inline-wrapper">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+          <Header/>
+        <div className="side-bar">side-bar</div>
+          <Main/>
+          <Footer/>
       </div>
     </div>
   );

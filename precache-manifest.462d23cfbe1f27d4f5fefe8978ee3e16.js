8f8b38434722605128c06d50257f092e349024b9
Anton <antonbendrik1993@gmail.com>
2020-03-11 15:13:27 +0200

    Updates

---
+++ precache-manifest.462d23cfbe1f27d4f5fefe8978ee3e16.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01dbb72077911fbfab68878c9f29b82",
+    "url": "/banzai-shop/index.html"
+  },
+  {
+    "revision": "b29380f3040eb2ac52cc",
+    "url": "/banzai-shop/static/css/main.96408cda.chunk.css"
+  },
+  {
+    "revision": "bc3321af22dc1e930cc6",
+    "url": "/banzai-shop/static/js/2.ba0fc9d3.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "/banzai-shop/static/js/2.ba0fc9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29380f3040eb2ac52cc",
+    "url": "/banzai-shop/static/js/main.8e0798a8.chunk.js"
+  },
+  {
+    "revision": "dee803f71a83d380beac",
+    "url": "/banzai-shop/static/js/runtime-main.a1bd7806.js"
+  },
+  {
+    "revision": "150ef6eecf52da116e5e5625575628a0",
+    "url": "/banzai-shop/static/media/apple-watch.150ef6ee.png"
+  },
+  {
+    "revision": "40fe233f45a96ac6d4b736af2c15611f",
+    "url": "/banzai-shop/static/media/iPhoneX.40fe233f.png"
+  },
+  {
+    "revision": "ed4ed4d30a1f8e4f2aecd0a0b4f84632",
+    "url": "/banzai-shop/static/media/slide-phone.ed4ed4d3.jpg"
+  },
+  {
+    "revision": "4da27e8681ba73fe31fa4709b5b3bdf7",
+    "url": "/banzai-shop/static/media/slide-phone2.4da27e86.jpg"
+  },
+  {
+    "revision": "6f00e1a18006025c5a4ef25ff5ce8613",
+    "url": "/banzai-shop/static/media/slide-tv.6f00e1a1.jpg"
+  }
+]);

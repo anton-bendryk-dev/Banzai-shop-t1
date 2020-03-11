dbe5c27753561fb605ec174db4a775c5933c797e
Anton <antonbendrik1993@gmail.com>
2020-03-11 15:14:50 +0200

    finished adaptabilyti

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { rootReducer } from './store/rootReducer';
@@ -13,9 +13,9 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>, document.getElementById('root'));
 
 
